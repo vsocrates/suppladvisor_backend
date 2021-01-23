6dee4569771bb138ee9cf49e091599cbffe0d118
Vimig Socrates <vimig.socrates@gmail.com>
2021-01-23 14:04:41 -0600

    add bs4 and scrapy libs

--- main.py
+++ main.py
@@ -1,20 +1,10 @@
 from flask import request, url_for
 from flask_api import FlaskAPI, status, exceptions
+import requests
+
 
 app = FlaskAPI(__name__)
 
-
-notes = {
-    0: 'do the shopping',
-    1: 'build the codez',
-    2: 'paint the door',
-}
-
-def note_repr(key):
-    return {
-        'url': request.host_url.rstrip('/') + url_for('notes_detail', key=key),
-        'text': notes[key]
-    }
 
 
 @app.route("/", methods=['GET', 'POST'])
@@ -31,25 +21,6 @@ def notes_list():
     # request.method == 'GET'
     return [note_repr(idx) for idx in sorted(notes.keys())]
 
-
-@app.route("/<int:key>/", methods=['GET', 'PUT', 'DELETE'])
-def notes_detail(key):
-    """
-    Retrieve, update or delete note instances.
-    """
-    if request.method == 'PUT':
-        note = str(request.data.get('text', ''))
-        notes[key] = note
-        return note_repr(key)
-
-    elif request.method == 'DELETE':
-        notes.pop(key, None)
-        return '', status.HTTP_204_NO_CONTENT
-
-    # request.method == 'GET'
-    if key not in notes:
-        raise exceptions.NotFound()
-    return note_repr(key)
 
 
 if __name__ == "__main__":
